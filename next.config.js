a97698177229a071f43439e8e9722a012b342f31
Ryan Fitzer <ryan@ryanfitzer.com>
2019-02-07 18:21:01 -0800

    Adding notes.

--- next.config.js
+++ next.config.js
@@ -6,19 +6,6 @@ module.exports = {
     // https://github.com/zeit/next.js/#disabling-file-system-routing
     useFileSystemPublicRoutes: false,
 
-    // onDemandEntries: {
-    //     // period (in ms) where the server will keep pages in the buffer
-    //     maxInactiveAge: 25 * 1000,
-    //     // number of pages that should be kept simultaneously without being disposed
-    //     pagesBufferLength: 2,
-    //     // optionally configure a port for the onDemandEntries WebSocket, not needed by default
-    //     websocketPort: 3001,
-    //     // optionally configure a proxy path for the onDemandEntries WebSocket, not need by default
-    //     websocketProxyPath: '/hmr',
-    //     // optionally configure a proxy port for the onDemandEntries WebSocket, not need by default
-    //     websocketProxyPort: 7002
-    // },
-
     // https://github.com/zeit/next.js/#customizing-webpack-config
     webpack: ( config ) => {
 
